0e06c65262500f86f8af4c77cbcf06e0f95d9dc4
Jason Stitt <js@jasonstitt.com>
2023-03-26 21:25:59 -0500

    Implement extension and in-place options

--- src/main.ts
+++ src/main.ts
@@ -8,9 +8,20 @@ import { walkFiles } from './walk'
 import { getOpenAiToken } from './token'
 import { runCompletionWithBackoff } from './completion'
 
+class UserError extends Error {}
+
 program
   .action(async (basedir: string, opts: any) => {
     try {
+      // Validate options
+      if (opts.inPlace && opts.extension) {
+        throw new UserError('Cannot use both --in-place and --extension')
+      } else if (opts.inPlace && opts.outputDir) {
+        throw new UserError('Cannot use both --in-place and --output-dir')
+      } else if (!opts.inPlace && !opts.extension) {
+        opts.extension = '.promptform'
+      }
+      // Process files
       const files = walkFiles(basedir || '.', opts.include, opts.omit)
       console.error(`Found ${files.length} files to process`)
       const token = await getOpenAiToken(opts.tokenFile)
@@ -19,11 +30,12 @@ program
       }
       const configuration = new Configuration({ apiKey: token })
       const openai = new OpenAIApi(configuration)
+      const extension = opts.extension || ''
       await bluebird.map(files, async (file: string) => {
         const filePath = path.resolve(basedir, file)
         const textInput = await fs.readFile(filePath, 'utf-8')
         const textOutput = await runCompletionWithBackoff(openai, `${opts.prompt}\n${textInput}`)
-        const outputFilename = path.resolve(opts.outputDir || basedir, file) + '.out'
+        const outputFilename = path.resolve(opts.outputDir || basedir, file) + extension
         if (opts.outputDir) {
           const outputDirPath = path.dirname(outputFilename)
           await mkdirp(outputDirPath)
@@ -33,7 +45,10 @@ program
       }, { concurrency: Number(opts.concurrency) })
     } catch (error: any) {
       let errorText = error.stack
-      if (error.response) {
+      if (error instanceof UserError) {
+        console.error(error.message)
+        process.exit(1)
+      } else if (error.response) {
         errorText = JSON.stringify(error.response.data, null, 2)
       }
       console.error(`There was an unexpected error (it crashed)\n\nThe details are:\n${errorText}`)
